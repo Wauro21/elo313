fe2d1b9133771ed9a45faa14bbac4d0e79e6754f
Mauricio <maurio.aravena@sansano.usm.cl>
2021-07-20 20:54:45 -0400

    Change on the max/min

--- tareas/tarea_2/code/2_1.m
+++ tareas/tarea_2/code/2_1.m
@@ -10,10 +10,10 @@ plotPath = "../plot_out/";
 
 L = 8;
 n = 0:1:L+1;
-omega = 2*pi*(0:1/1000:1);
+omega = 2*pi*((0:1/1000:1)-0.5);
 H_w = exp(-j*omega*L/2).*(1/(L+1)).*(sin(omega*(L+1)/2))./sin(omega/2);
 
-name= "2_1"
+name= "2_1";
 figure
 	subplot(2,1,1)
 		plot(omega, abs(H_w), "color", Colors(1,:), "linewidth", 1.15);
@@ -21,13 +21,35 @@ figure
 		xlabel("Frecuencia normalizada \textit{rad/s}","Interpreter", "latex")
 		ylabel("Magnitud -")
 		title("Magnitud respuesta en frecuencia H(\omega)")
-		xlim([0, 2*pi])
+		xlim([-pi, pi])
+		hold on
+		for i=1:1:4
+			%Minimos
+			local_omega = 2*pi*i/9;
+			plot(local_omega, (1/(L+1)).*(sin(local_omega*(L+1)/2))./sin(local_omega/2),"b*")
+		end
+		
+		for i=1:1:5
+			%maximos
+			if(i == 1)
+				plot(0, 1,"g*")
+			else
+				local_omega = pi*(1+2*(i-1))/9;
+				plot(local_omega, abs((1/(L+1)).*(sin(local_omega*(L+1)/2))./sin(local_omega/2)),"g*")
+			end
+		end
+		hold off
 	subplot(2,1,2)
 		plot(omega, unwrap(angle(H_w)), "color", Colors(2,:), "linewidth", 1.15);
 		grid on
 		xlabel("Frecuencia normalizada \textit{rad/s}","Interpreter", "latex")
 		ylabel("Fase \textit{rad}","Interpreter", "latex")
 		title("Fase respuesta en frecuencia H(\omega)")
-		xlim([0, 2*pi])
+		xlim([-pi, pi])
+		hold on
+		local_omega = -2*pi/9;
+		plot(local_omega, unwrap(angle(exp(-j*local_omega*L/2).*(1/(L+1)).*(sin(local_omega*(L+1)/2))./sin(local_omega/2))),"g*")
+		local_omega = 2*pi/9;
+		plot(local_omega, unwrap(angle(exp(-j*local_omega*L/2).*(1/(L+1)).*(sin(local_omega*(L+1)/2))./sin(local_omega/2))),"b*")
 cfg.PaperPositionMode = 'auto';
 print(plotPath+name,'-dpdf','-bestfit');
